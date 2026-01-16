c293d8cc03cd0aa1c5eff26ff84a61227472d5ef
darcrand <darcrandex@gmail.com>
2026-01-16 12:21:33 +0800

    🐞 fix(home): check login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,28 @@
 import { getWorkfows } from '@/actions/workflow'
 import { useUserInfo } from '@/query/useUserInfo'
 import { useQuery } from '@tanstack/react-query'
+import { App } from 'antd'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
+  const router = useRouter()
+  const { message } = App.useApp()
   const userInfoQuery = useUserInfo()
 
   const workflowsQuery = useQuery({
     queryKey: ['workflows'],
     queryFn: getWorkfows,
   })
+
+  const onOpenWorkflow = (id: string) => {
+    if (userInfoQuery.isError) {
+      message.error('请先登录')
+      router.push('/login')
+      return
+    }
+    router.push(`/workflow/${id}`)
+  }
 
   return (
     <>
@@ -28,8 +41,12 @@ export default function Home() {
 
       <ol className='m-6 list-decimal'>
         {workflowsQuery.data?.map((item) => (
-          <li key={item.id}>
-            <Link href={`/workflow/${item.id}`}>{item.name}</Link>
+          <li
+            key={item.id}
+            className='cursor-pointer underline hover:text-blue-500'
+            onClick={() => onOpenWorkflow(item.id)}
+          >
+            {item.name}
           </li>
         ))}
       </ol>
